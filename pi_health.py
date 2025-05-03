d699a39c1281a40fd22ee3f23bb1aefa0b07ff53
peremil <peremilsoderstom2@hotmail.com>
2025-05-03 17:37:58 +0200

    added single quotesin the middle of f-string

--- pi_health.py
+++ pi_health.py
@@ -3,4 +3,4 @@ import os
 #temp = os.system("vcgencmd measure_temp")
 #uptime = os.system("uptime")
 
-print(f"This pi has been running for: {os.system("uptime")} and the temperature is: {os.system("vcgencmd measure_temp")}.")
+print(f"This pi has been running for: {os.system('uptime')} and the temperature is: {os.system('vcgencmd measure_temp')}.")
